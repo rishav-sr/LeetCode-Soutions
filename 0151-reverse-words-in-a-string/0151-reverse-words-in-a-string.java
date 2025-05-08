805f83a927baab2ca9fd310f065dfd78a43caeee
rishav-sr <96995722+rishav-sr@users.noreply.github.com>
2025-05-09 00:47:47 +0530

    Time: 7 ms (57.63%), Space: 44.7 MB (24.77%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,26 +1,39 @@
 class Solution {
-    public String reverseWords(String s) {
-        s=s+" ";
-        StringBuilder result= new StringBuilder();
-        StringBuilder word= new StringBuilder();
-        for(int i=0;i<s.length();i++) {
-            if(Character.isLetterOrDigit(s.charAt(i)))
-            word.append(s.charAt(i));
-            else
-            {
-                System.out.println(word);
-                if(word.isEmpty())
-                {
-                    continue;
-                }
-                else {
+    // public String reverseWords(String s) {
+    //     s=s+" ";
+    //     StringBuilder result= new StringBuilder();
+    //     StringBuilder word= new StringBuilder();
+    //     for(int i=0;i<s.length();i++) {
+    //         if(Character.isLetterOrDigit(s.charAt(i)))
+    //         word.append(s.charAt(i));
+    //         else
+    //         {
+    //             System.out.println(word);
+    //             if(word.isEmpty())
+    //             {
+    //                 continue;
+    //             }
+    //             else {
                     
-                    word.append(" ");
-                    result.insert(0,word);
-                    word= new StringBuilder();
-                }
-            }
+    //                 word.append(" ");
+    //                 result.insert(0,word);
+    //                 word= new StringBuilder();
+    //             }
+    //         }
+    //     }
+    //     return result.toString().trim();
+    // }
+    public String reverseWords(String s) {
+        Stack<String> st= new Stack<>();
+        for(String a:s.trim().split(" "))
+        {
+            if(!a.isBlank())
+            st.push(a);
         }
-        return result.toString().trim();
+        StringBuilder sb= new StringBuilder();
+        while(!st.isEmpty()) {
+            sb.append(st.pop()+" ");
+        }
+        return sb.toString().trim();
     }
 }
