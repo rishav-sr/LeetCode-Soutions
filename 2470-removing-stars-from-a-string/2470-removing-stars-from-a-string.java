dc1074bc4f297d7fc87f83102bb840bca0e61d1d
rishav-sr <96995722+rishav-sr@users.noreply.github.com>
2025-05-14 18:11:46 +0530

    Time: 32 ms (84.4%), Space: 45.7 MB (80.26%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.java
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.java
@@ -1,23 +1,34 @@
 class Solution {
+    // public String removeStars(String s) {
+    //     Stack<Character> stack= new Stack<>();
+    //     StringBuilder sb= new StringBuilder();
+    //     for(char ch:s.toCharArray()) {
+    //         if(ch=='*') {
+    //             stack.pop();
+    //         }
+    //         else {
+    //             stack.push(ch);
+    //         }
+    //     }
+    //     if(stack.isEmpty())
+    //     return "";
+    //     else
+    //     {
+    //         while(!stack.isEmpty()) {
+    //             sb.append(stack.pop());
+    //         }
+    //     }
+    //     return sb.reverse().toString();
+    // }
+
     public String removeStars(String s) {
-        Stack<Character> stack= new Stack<>();
         StringBuilder sb= new StringBuilder();
         for(char ch:s.toCharArray()) {
-            if(ch=='*') {
-                stack.pop();
-            }
-            else {
-                stack.push(ch);
-            }
+            if(ch=='*')
+            sb.deleteCharAt(sb.length()-1);
+            else
+            sb.append(ch);
         }
-        if(stack.isEmpty())
-        return "";
-        else
-        {
-            while(!stack.isEmpty()) {
-                sb.append(stack.pop());
-            }
-        }
-        return sb.reverse().toString();
+        return sb.toString();
     }
 }
