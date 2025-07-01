f99e7aaaeaad173c62fa414015e1a6a5c72ca581
rishav-sr <96995722+rishav-sr@users.noreply.github.com>
2025-07-01 20:44:59 +0530

    Time: 1 ms (99.49%), Space: 44.3 MB (42.05%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,20 +1,34 @@
 class Solution {
     public int compress(char[] chars) {
-        int i = 0, res = 0;
-        while (i < chars.length) {
-            int groupLength = 1;
-            while (i + groupLength < chars.length && chars[i + groupLength] == chars[i]) {
-                groupLength++;
-            }
-            chars[res++] = chars[i];
-            if (groupLength > 1) {
-                for (char ch : Integer.toString(groupLength).toCharArray()) {
-                    chars[res++] = ch;
+        int start = 0;
+        int count = 0;
+        int i = 0;
+        for (i = 0; i < chars.length - 1; i++) {
+            if (chars[i] == chars[i + 1]) {
+                count++;
+            } else {
+                if (count == 0) {
+                    chars[start++] = chars[i];
+                } else {
+                    String add = Integer.toString(count + 1);
+                    chars[start++] = chars[i];
+                    for (char t : add.toCharArray()) {
+                        chars[start++] = t;
+                    }
                 }
+                count = 0;
             }
-
-            i += groupLength;
         }
-        return res;
+
+        if (count == 0) {
+            chars[start++] = chars[i];
+        } else {
+            String add = Integer.toString(count + 1);
+            chars[start++] = chars[i];
+            for (char t : add.toCharArray()) {
+                chars[start++] = t;
+            }
+        }
+        return start;
     }
 }
